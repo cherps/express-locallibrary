8074c3a614ca5017b10caa4860286a716ef9f57f
Cherps <cherps@gmail.com>
2020-06-24 10:55:04 +0800

    Initial commit (Tutorial Part 4 completed)

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, minlength: 3, maxlength: 100},
+  }
+)
+
+GenreSchema
+.virtual('url')
+.get(() => {
+  return '/genre/' + this._id;
+})
+
+module.exports = mongoose.model('Genre', GenreSchema);
